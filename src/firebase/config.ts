342add3d0fc8c762b54fbab2492e85b5ff35519a
Raushan Kumar Ray <raushankumarray96@gmail.com>
2026-03-26 14:28:33 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   "appId": "1:1032544630918:web:8120314f9d3fe9667a3907",
   "apiKey": "AIzaSyBuqufh5wEqoPWJrRN3gTijATr_MvlrIYQ",
   "authDomain": "studio-5851719114-7ea95.firebaseapp.com",
-  "measurementId": "",
+  "measurementId": "G-G9ERME65JC",
   "messagingSenderId": "1032544630918"
 };
